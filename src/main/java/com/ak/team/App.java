17db308bdf74f9ae688dfb6f2e74d5cf68eed661
Krzysiek K <kkosecki@gmail.com>
2014-10-20 23:24:28 +0200

    Issue 1:Add Spring Security support

---
+++ src/main/java/com/ak/team/App.java
@@ -0,0 +1,41 @@
+package com.ak.team;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.authentication.UserCredentials;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class App extends SpringBootServletInitializer {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+    
+    @Override
+    public void onStartup(ServletContext servletContext)
+            throws ServletException {
+        super.onStartup(servletContext);
+        LOG.info("------TEST-----------");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+    
+    public static void main(String[] args) throws Exception {
+        ApplicationContext ctx = SpringApplication.run(App.class);
+    }
+}
