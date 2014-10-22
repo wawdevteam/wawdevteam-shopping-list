0857c2b77acc9d30f064fa155f2c229152809809
Artur K <art.kluz@gmail.com>
2014-10-22 23:03:17 +0200

    Issue 6:  Configure OpenShift cartridge with Java 8 support - action hooks pt.3

--- src/main/java/com/ak/team/App.java
+++ src/main/java/com/ak/team/App.java
@@ -9,33 +9,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.authentication.UserCredentials;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class App extends SpringBootServletInitializer {
-    
-    private static final Logger LOG = LoggerFactory.getLogger(App.class);
-    
-    @Override
-    public void onStartup(ServletContext servletContext)
-            throws ServletException {
-        super.onStartup(servletContext);
-        LOG.info("------TEST-----------");
-    }
 
-    @Override
-    protected SpringApplicationBuilder configure(
-            SpringApplicationBuilder application) {
-        return application.sources(App.class);
-    }
-    
-    public static void main(String[] args) throws Exception {
-        ApplicationContext ctx = SpringApplication.run(App.class);
-    }
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+		LOG.info("------TEST-----------");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class);
+	}
 }
